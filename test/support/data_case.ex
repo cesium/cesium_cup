156d451eb562a85603583744cb63017d05f1ae9c
Nelson Estevão <nelsonmestevao@proton.me>
2022-07-09 17:23:30 +0100

    Add README and CONTRIBUTING guide

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -16,6 +16,8 @@ defmodule CesiumCup.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL
+
   using do
     quote do
       alias CesiumCup.Repo
@@ -28,8 +30,8 @@ defmodule CesiumCup.DataCase do
   end
 
   setup tags do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(CesiumCup.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = SQL.Sandbox.start_owner!(CesiumCup.Repo, shared: not tags[:async])
+    on_exit(fn -> SQL.Sandbox.stop_owner(pid) end)
     :ok
   end
 
