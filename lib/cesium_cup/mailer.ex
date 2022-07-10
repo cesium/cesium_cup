92019ca6bc7824a23bdf2389c184bdfb895212dd
Nelson Estevão <nelsonmestevao@gmail.com>
2022-07-10 16:57:20 +0100

    Add README and CONTRIBUTING (#3)

--- lib/cesium_cup/mailer.ex
+++ lib/cesium_cup/mailer.ex
@@ -1,3 +1,6 @@
 defmodule CesiumCup.Mailer do
+  @moduledoc """
+  A mailer module providing functionality to deliver emails.
+  """
   use Swoosh.Mailer, otp_app: :cesium_cup
 end
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CesiumCup.MixProject do
     [
       app: :cesium_cup,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -33,22 +33,43 @@ defmodule CesiumCup.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      # web
       {:phoenix, "~> 1.6.6"},
+      {:phoenix_html, "~> 3.0"},
+      {:phoenix_live_view, "~> 0.17.5"},
+      {:plug_cowboy, "~> 2.5"},
+
+      # database
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.6"},
-      {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
+
+      # mailer
       {:swoosh, "~> 1.3"},
+
+      # i18n
+      {:gettext, "~> 0.18"},
+
+      # monitoring
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.18"},
+      {:phoenix_live_dashboard, "~> 0.6"},
+
+      # utilities
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+
+      # testing
+      {:floki, ">= 0.30.0", only: :test},
+
+      # development
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
+
+      # tools
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
+      {:sobelow, "~> 0.11", only: :dev, runtime: false}
     ]
   end
 
@@ -61,7 +82,8 @@ defmodule CesiumCup.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.seed": ["run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": ["esbuild default --minify", "phx.digest"]
